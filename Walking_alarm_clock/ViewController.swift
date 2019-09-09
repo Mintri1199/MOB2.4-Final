83699f8429c549e0d7aad4be0d8b63675319147e
Jackson Ho <mintri1199@gmail.com>
2019-09-09 16:09:47 -0700

    [Added] initial commit

---
+++ Walking_alarm_clock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Walking_alarm_clock
+//
+//  Created by Jackson Ho on 9/9/19.
+//  Copyright © 2019 Jackson Ho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ Walking_alarm_clockTests/Walking_alarm_clockTests.swift
@@ -0,0 +1,34 @@
+//
+//  Walking_alarm_clockTests.swift
+//  Walking_alarm_clockTests
+//
+//  Created by Jackson Ho on 9/9/19.
+//  Copyright © 2019 Jackson Ho. All rights reserved.
+//
+
+import XCTest
+@testable import Walking_alarm_clock
+
+class Walking_alarm_clockTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
